b1ce363c492bbdc234f0974e58f9c4cc656ec4f1
Samuel Meier <samuel.meier@students.unibe.ch>
2021-08-10 10:03:54 +0200

    initialized R project for rifreg packagae

---
+++ R/rifreg.R
@@ -0,0 +1,36 @@
+#################################################################################
+################# Recentered Influence Function Regression ######################
+#################################################################################
+
+# Author: David Gallusser & Samuel Meier
+# Date:   10.08.2021
+
+# Introduction ------------------------------
+
+# This script provides and describes functions to calculate recentered influence
+# function regressions.
+
+
+# Packages ------------------------------------
+
+# The following packages are necessary dependencies of this script.
+
+#' @import here
+#' @import assertthat
+#' @importFrom stats approx
+
+
+# Data -------------------------------------
+
+# The following data is stored in the /data-directory
+
+# data('skala44_2015', envir = environment())
+# data('skala44_2019', envir = environment())
+# data('skala44_2021', envir = environment())
+
+
+# Recentered Influence Functions -----------------------------
+
+
+
+
